7a4f6f35629d96f4636d8bcf42517f417b770118
aakanksha1 <aakanksha720@gmail.com>
2020-03-16 14:39:53 -0500

    app test

--- src/App.test.js
+++ src/App.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+test('App is rendered', () => {
+    // const { getByTestId } = render(<App />);
+    // const list = getByTestId("listings-grid");
+    // expect(list).toBeInTheDocument()
+})
