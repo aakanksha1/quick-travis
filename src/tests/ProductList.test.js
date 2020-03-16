41d76678eb64ae7256b8c1fdc3bbd47c8fd9f566
aakanksha1 <aakanksha720@gmail.com>
2020-03-16 14:32:30 -0500

    testing CI and CD

--- src/tests/ProductList.test.js
+++ src/tests/ProductList.test.js
@@ -2,7 +2,7 @@ import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import App from '../App.js'
 
-test('ListingList is rendered', () => {
+test('ProductList is rendered', () => {
     const { getByTestId } = render(<App />);
     const list = getByTestId("listings-grid");
     expect(list).toBeInTheDocument()
